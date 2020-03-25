557f9268e892ea55c44d60493e8901f1886b8c3d
Kshitij Morodia <kshitijmorodia@gmail.com>
2020-03-25 19:55:07 +0530

    standard login and registration implemented

--- src/personal/views.py
+++ src/personal/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_screen_view(request):
-	print(request.headers)
+	#print(request.headers)
 	return render(request, "personal/home.html", {})
